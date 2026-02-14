a440b395c67e174a2b1a7504af09c2ce2a162aec
Krish-01-code <krishkushwaha.w@gmail.com>
2026-02-14 02:22:14 -0600

    Scalar Arithmetic

---
+++ Python_numpy5.py
@@ -0,0 +1,19 @@
+#Performing Scalar arithmetic on numpy arrays
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5])
+
+sum = arr + 1
+diff = arr - 1
+product = arr * 2
+div = arr/4
+power = arr ** 2
+mod = arr % 2
+
+print(sum)
+print(diff)
+print(product)
+print(div)
+print(power)
+print(mod)
